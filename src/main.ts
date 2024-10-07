5d009f0e5c1060b995ae7d3b48264290cb7aa255
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-07 14:40:59 -0700

    Step 1 Complete
    change game name (step 0), add emoji button (step 1)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+// ---------------------------------------------- CONTENT
+
+const gameName = "CLICKING TIME";
 document.title = gameName;
+
+const buttonContent = "☕";
+
+// ---------------------------------------------- ASSEMBLY
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = buttonContent;
+app.append(button);
