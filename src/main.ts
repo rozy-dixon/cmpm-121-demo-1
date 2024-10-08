c5fbfb7214cc6f1e24b3a596005577e596ef958c
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-08 06:47:02 -0700

    Step 4 complete
    convert increase from setInterval() to requestAnimationFrame()

--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,30 @@ document.title = gameName;
 let counter: number = 0;
 let unitLabel = "coffees deep";
 
-function increaseCounter() {
-    counter += 1;
-    if (counter == 1) {
-        unitLabel = "coffee deep";
-    } else {
-        unitLabel = "coffees deep";
-    }
-    counterDisplay.innerHTML = `${counter} ${unitLabel}`;
+function increaseCounter(x: number) {
+  counter += x;
+  if (Math.floor(counter) == 1) {
+    unitLabel = "coffee deep";
+  } else {
+    unitLabel = "coffees deep";
+  }
+  counterDisplay.innerHTML = `${Math.floor(counter)} ${unitLabel}`;
 }
 
 const buttonContent = "☕";
+
+let curPerformance, fps;
+let prevPerformance = performance.now();
+function step() {
+  // src = https://stackoverflow.com/questions/4787431/how-do-i-check-framerate-in-javascript
+  // src = https://chat.brace.tools/s/34982369-88e5-4e6c-abce-bc25002d522e
+  curPerformance = performance.now();
+  fps = 1000 / (curPerformance - prevPerformance);
+  prevPerformance = curPerformance;
+
+  increaseCounter(1 / fps);
+  requestAnimationFrame(step);
+}
 
 // ---------------------------------------------- ASSEMBLY
 
@@ -37,7 +50,7 @@ button.innerHTML = buttonContent;
 app.append(button);
 
 button.addEventListener("click", () => {
-    increaseCounter();
+  increaseCounter(1);
 });
 
-setInterval(increaseCounter, 1000);
+requestAnimationFrame(step);
