16945c8e496d25fbd5a17d3940869c9b016f7207
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-07 17:56:52 -0700

    Step 3 complete
    per second increase implementation

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,16 @@ document.title = gameName;
 let counter: number = 0;
 let unitLabel = "coffees deep";
 
+function increaseCounter() {
+    counter += 1;
+    if (counter == 1) {
+        unitLabel = "coffee deep";
+    } else {
+        unitLabel = "coffees deep";
+    }
+    counterDisplay.innerHTML = `${counter} ${unitLabel}`;
+}
+
 const buttonContent = "☕";
 
 // ---------------------------------------------- ASSEMBLY
@@ -27,11 +37,7 @@ button.innerHTML = buttonContent;
 app.append(button);
 
 button.addEventListener("click", () => {
-    counter += 1;
-    if (counter == 1) {
-        unitLabel = "coffee deep"
-    } else {
-        unitLabel = "coffees deep"
-    }
-    counterDisplay.innerHTML = `${counter} ${unitLabel}`;
-})
+    increaseCounter();
+});
+
+setInterval(increaseCounter, 1000);
