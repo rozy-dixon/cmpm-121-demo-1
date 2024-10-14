1c2f5acc01ca81205d07e2457cb39dd3a7421500
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-14 15:35:38 -0700

    Step 6 Complete
    more upgrade options, cleanup, amended commit

--- src/main.ts
+++ src/main.ts
@@ -2,50 +2,33 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-// ---------------------------------------------- DESIGN
+// ---------------------------------------------- INITS
 
-/* 
-- upgrade 1: auto-clicking
-*/
-
-// ---------------------------------------------- CONTENT
-
-// initialize name display
-const gameName = "Caffeine Clicker";
-document.title = gameName;
-
-// initialize counter display defaults
 let counter: number = 0;
-let unitLabel = "coffees deep";
-let growthRate = 0;
+let growthRate: number = 0;
+const growthRateSuffix: string = "coffees/second";
 
-// allow for counter increase
-function increaseCounter(x: number) {
-  counter += x;
-  if (Math.floor(counter) == 1) {
-    unitLabel = "coffee deep";
-  } else {
-    unitLabel = "coffees deep";
-  }
-  counterDisplay.innerHTML = `${Math.floor(counter)} ${unitLabel}`;
+const fix: number = 2;
+
+const upgradeACost: number = 10;
+const upgradeBCost: number = 100;
+const upgradeCCost: number = 1000;
+
+const upgradeAIncrease: number = 0.1;
+const upgradeBIncrease: number = 2.0;
+const upgradeCIncrease: number = 50.0;
+
+let upgradeACount: number = 0;
+let upgradeBCount: number = 0;
+let upgradeCCount: number = 0;
+
+// ---------------------------------------------- FUNCTIONS
+
+function setCounter(difference: number = 1) {
+  counter += difference;
+  counterDisplay.innerHTML = `${counter.toFixed(fix)}`;
 }
 
-// allow for counter decrease
-function decreaseCounter(x: number) {
-  counter -= x;
-  if (Math.floor(counter) == 1) {
-    unitLabel = "coffee deep";
-  } else {
-    unitLabel = "coffees deep";
-  }
-  counterDisplay.innerHTML = `${Math.floor(counter)} ${unitLabel}`;
-}
-
-// initialize button contents
-const buttonContent = "☕";
-const upgrade1Content = "purchase upgrade";
-
-// allow for upgrade 1
 let curPerformance, fps;
 let prevPerformance = performance.now();
 function step() {
@@ -55,41 +38,81 @@ function step() {
   fps = 1000 / (curPerformance - prevPerformance);
   prevPerformance = curPerformance;
 
-  upgrade1Button.disabled = counter < 10;
+  upgradeAButton.disabled = counter < upgradeACost;
+  upgradeBButton.disabled = counter < upgradeBCost;
+  upgradeCButton.disabled = counter < upgradeCCost;
 
-  increaseCounter(growthRate / fps);
+  upgradeAButton.innerHTML = `A: ${upgradeACount}`;
+  upgradeBButton.innerHTML = `B: ${upgradeBCount}`;
+  upgradeCButton.innerHTML = `C: ${upgradeCCount}`;
+
+  growthRateDisplay.innerHTML = `${growthRate.toFixed(fix)} ${growthRateSuffix}`;
+
+  setCounter(growthRate / fps);
   requestAnimationFrame(step);
 }
 
-function purshaseUpgrade1() {
-  decreaseCounter(10);
-  growthRate += 1;
-}
+// ---------------------------------------------- TITLE DISPLAY
 
-// ---------------------------------------------- ASSEMBLY
+const gameName = "Caffeine Clicker";
+document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// ---------------------------------------------- COUNTER AND GROWTH DISPLAY
+
 const counterDisplay = document.createElement("div");
-counterDisplay.innerHTML = `${counter} ${unitLabel}`;
+counterDisplay.innerHTML = `${counter}`;
 app.append(counterDisplay);
+
+const growthRateDisplay = document.createElement("div");
+growthRateDisplay.innerHTML = `${growthRate} ${growthRateSuffix}`;
+app.append(growthRateDisplay);
+
+// ---------------------------------------------- BUTTON DISPLAY
 
 requestAnimationFrame(step);
 
-const button = document.createElement("button");
-button.innerHTML = buttonContent;
-app.append(button);
+const clickContent = "☕";
+const clickButton = document.createElement("button");
+clickButton.innerHTML = clickContent;
+app.append(clickButton);
 
-button.addEventListener("click", () => {
-  increaseCounter(1);
+clickButton.addEventListener("click", () => {
+  setCounter();
 });
 
-const upgrade1Button = document.createElement("button");
-upgrade1Button.innerHTML = upgrade1Content;
-app.append(upgrade1Button);
+const upgradeAContent = `A: ${upgradeACount}`;
+const upgradeAButton = document.createElement("button");
+upgradeAButton.innerHTML = upgradeAContent;
+app.append(upgradeAButton);
 
-upgrade1Button.addEventListener("click", () => {
-  purshaseUpgrade1();
+upgradeAButton.addEventListener("click", () => {
+  setCounter(-upgradeACost);
+  growthRate += upgradeAIncrease;
+  upgradeACount++;
+});
+
+const upgradeBContent = `B: ${upgradeBCount}`;
+const upgradeBButton = document.createElement("button");
+upgradeBButton.innerHTML = upgradeBContent;
+app.append(upgradeBButton);
+
+upgradeBButton.addEventListener("click", () => {
+  setCounter(-upgradeBCost);
+  growthRate += upgradeBIncrease;
+  upgradeBCount++;
+});
+
+const upgradeCContent = `C: ${upgradeCCount}`;
+const upgradeCButton = document.createElement("button");
+upgradeCButton.innerHTML = upgradeCContent;
+app.append(upgradeCButton);
+
+upgradeCButton.addEventListener("click", () => {
+  setCounter(-upgradeCCost);
+  growthRate += upgradeCIncrease;
+  upgradeCCount++;
 });
