20d8afad0ec88665be9c309ec2525b039fa0c592
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-29 10:44:51 -0700

    D1 C revisions

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ const fix: number = 2;
 
 interface Item {
   name: string;
+  id: string;
   cost: number;
   rate: number;
   count: number;
@@ -27,6 +28,7 @@ interface Item {
 const availableItems: Item[] = [
   {
     name: "double shot",
+    id: "double-shot",
     cost: 10,
     rate: 0.1,
     count: 0,
@@ -34,6 +36,7 @@ const availableItems: Item[] = [
   },
   {
     name: "triple shot",
+    id: "triple-shot",
     cost: 100,
     rate: 2,
     count: 0,
@@ -41,6 +44,7 @@ const availableItems: Item[] = [
   },
   {
     name: "quadruple shot",
+    id: "quadruple-shot",
     cost: 1000,
     rate: 50,
     count: 0,
@@ -48,6 +52,7 @@ const availableItems: Item[] = [
   },
   {
     name: "quintuple shot",
+    id: "quintuple-shot",
     cost: 10000,
     rate: 1000,
     count: 0,
@@ -55,6 +60,7 @@ const availableItems: Item[] = [
   },
   {
     name: "sextuple shot",
+    id: "sextuple-shot",
     cost: 100000,
     rate: 10000,
     count: 0,
@@ -64,35 +70,39 @@ const availableItems: Item[] = [
 
 // ---------------------------------------------- FUNCTIONS
 
-function setCounter(difference: number = 1) {
+function SetCounter(difference: number = 1) {
   counter += difference;
   counterDisplay.innerHTML = `${counter.toFixed(fix)}`;
 }
 
-let curPerformance, fps;
-let prevPerformance = performance.now();
-function step() {
+let currentPerformance, fps;
+let previousPerformance = performance.now();
+function Step() {
   // src = https://stackoverflow.com/questions/4787431/how-do-i-check-framerate-in-javascript
   // src = https://chat.brace.tools/s/34982369-88e5-4e6c-abce-bc25002d522e
-  curPerformance = performance.now();
-  fps = 1000 / (curPerformance - prevPerformance);
-  prevPerformance = curPerformance;
+  currentPerformance = performance.now();
+  fps = 1000 / (currentPerformance - previousPerformance);
+  previousPerformance = currentPerformance;
 
   Array.from(document.getElementsByTagName("button")).forEach(
     (upgradeButton) => {
-      availableItems.forEach((element) => {
-        if (upgradeButton.id === element.name) {
-          upgradeButton.disabled = counter < element.cost;
-          upgradeButton.innerHTML = `$${element.cost.toFixed(fix)} ${element.name} (${element.count})`;
-        }
-      });
+      DisableButton(upgradeButton);
     },
   );
 
   growthRateDisplay.innerHTML = `${growthRate.toFixed(fix)} ${growthRateSuffix}`;
 
-  setCounter(growthRate / fps);
-  requestAnimationFrame(step);
+  SetCounter(growthRate / fps);
+  requestAnimationFrame(Step);
+}
+
+function DisableButton(upgradeButton: HTMLButtonElement) {
+  availableItems.forEach((element) => {
+    if (upgradeButton.id === element.id) {
+      upgradeButton.disabled = counter < element.cost;
+      upgradeButton.innerHTML = `$${element.cost.toFixed(fix)} ${element.name} (${element.count})`;
+    }
+  });
 }
 
 // ---------------------------------------------- TITLE DISPLAY
@@ -116,27 +126,28 @@ app.append(growthRateDisplay);
 
 // ---------------------------------------------- BUTTON DISPLAY
 
-requestAnimationFrame(step);
+requestAnimationFrame(Step);
 
 const clickContent = "☕";
 const clickButton = document.createElement("h2");
 clickButton.innerHTML = clickContent;
 app.append(clickButton);
 
-clickButton.addEventListener("click", () => {
-  setCounter();
+clickButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  SetCounter();
   // src = https://chat.brace.tools/s/306ec42f-7ac1-4381-be4b-dbcb47af314f
   clickButton.style.transform = `rotate(${(rotation += 360)}deg)`;
 });
 
 availableItems.forEach((element) => {
   const upgradeButton = document.createElement("button");
-  upgradeButton.id = element.name;
+  upgradeButton.id = element.id;
   upgradeButton.title = element.description;
   app.append(upgradeButton);
 
   upgradeButton.addEventListener("click", () => {
-    setCounter(-element.cost);
+    SetCounter(-element.cost);
     element.cost *= costMultiplier;
     growthRate += element.rate;
     element.count++;
